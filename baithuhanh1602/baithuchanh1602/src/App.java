f2e1360bfdea53ad6db71a96d8b2be3ba23dc6dc
Your username login githubtrung2901 <Your email githubthanhtrungg.2901@gmail.com>
2022-02-16 14:37:45 +0700

    baithuchanh1602

--- baithuhanh1602/baithuchanh1602/src/App.java
+++ baithuhanh1602/baithuchanh1602/src/App.java
@@ -1,5 +1,17 @@
-public class App {
+import java.util.Scanner;
+public class App30 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n, soDu, sum = 0;
+        System.out.println("Nhập số nguyên dương: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        while (n > 0) {
+            soDu = n % 10;
+            n = n / 10;
+            sum += soDu;
+        }
+             
+        System.out.println("Tổng các chữ số = " + sum);
+
     }
-}
+}   
