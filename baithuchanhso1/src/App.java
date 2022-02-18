d87332c27b9dad7c6aea87e455a8164ff6516bb5
Your username login githubtrung2901 <Your email githubthanhtrungg.2901@gmail.com>
2022-02-18 13:06:28 +0700

    baith1602

--- baithuchanhso1/src/App.java
+++ baithuchanhso1/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Java!");
+        //khai bao bien
+        double heSoA, heSoB, nghiemX;
+        //can thong bao cho nguoi dung nhap vao he so
+        System.out.println("Nhap he so A");  
+        //doc gia tri nguoi dung nhap vao tu ban phim => gan vao bien
+        //khai bao doi tuong Scanner de doc du lieu tu nguoi dung nhap vao
+        Scanner abc = new Scanner(System.in);
+        //gan gia tri nguoi dung nhap vao tu ban phim cho bien so heSoA
+        heSoA = abc.nextDouble();
+        //can thong bao cho nguoi dung nhap vao he so
+        System.out.println("Nhap he so B"); 
+        heSoB = abc.nextDouble();
+        nghiemX = -heSoB / heSoA;
+    
     }
 }
